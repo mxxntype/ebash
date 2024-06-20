953feef0af2b607a3fec744c7f3126bb8b031dc9
mxxntype <59417007+mxxntype@users.noreply.github.com>
2024-06-20 17:48:49 +0300

    feat: Implement a REPL loop

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,25 @@ const KNOWN_COMMANDS: &[&str] = &[];
 fn main() {
     let _ = color_eyre::install().inspect_err(|e| eprintln!("Could not install color-eyre: {e:?}"));
 
-    print!("{PROMPT}");
-    io::stdout().flush().unwrap();
-
     let stdin = io::stdin();
     let mut commandline = String::new();
-    stdin.read_line(&mut commandline).unwrap();
-    let commandline = commandline.trim();
 
-    if !KNOWN_COMMANDS.contains(&commandline) {
-        println!("{commandline}: command not found");
+    loop {
+        print!("{PROMPT}");
         io::stdout().flush().unwrap();
+
+        match stdin.read_line(&mut commandline) {
+            Ok(bytes) if bytes > 0 => {
+                commandline = commandline.trim().into();
+
+                if !KNOWN_COMMANDS.contains(&commandline.as_str()) {
+                    println!("{commandline}: command not found");
+                    io::stdout().flush().unwrap();
+                }
+            }
+            _ => break,
+        }
+
+        commandline.clear();
     }
 }
