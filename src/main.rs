5b19ca29b8e836cfe1f1d884d8830ae2f4a71b75
mxxntype <59417007+mxxntype@users.noreply.github.com>
2024-06-20 17:25:17 +0300

    feat: Implement unknown command handling

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
 use std::io::{self, Write};
 
+const PROMPT: &str = "$ ";
+const KNOWN_COMMANDS: &[&str] = &[];
+
 fn main() {
-    print!("$ ");
+    let _ = color_eyre::install().inspect_err(|e| eprintln!("Could not install color-eyre: {e:?}"));
+
+    print!("{PROMPT}");
     io::stdout().flush().unwrap();
 
-    // Wait for user input
     let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
+    let mut commandline = String::new();
+    stdin.read_line(&mut commandline).unwrap();
+    let commandline = commandline.trim();
+
+    if !KNOWN_COMMANDS.contains(&commandline) {
+        println!("{commandline}: commmand not found");
+    }
 }
